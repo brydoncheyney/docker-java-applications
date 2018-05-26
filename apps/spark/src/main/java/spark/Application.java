18af5631fef2040a18560949facae68c4a470f9b
Brydon Cheyney <brydon@remission.org.uk>
2018-05-26 23:02:28 +0100

    spark service loads properties file

--- apps/spark/src/main/java/spark/Application.java
+++ apps/spark/src/main/java/spark/Application.java
@@ -1,9 +1,23 @@
 package spark;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import static spark.Spark.get;
+import static spark.Spark.internalServerError;
 
 public class Application {
-    public static void main(String[] args) {
-        get("/", (req, res) -> "Spark!");
+  public static void main(String[] args) {
+    Properties properties = new Properties();
+    try {
+      properties.load(new FileInputStream("/opt/spark-service/spark-service.properties"));
+    } catch (IOException e) {
+      internalServerError((req, res) -> { return e.getMessage(); });
     }
+
+    get("/", (req, res) -> String.format("Spark! [%s|%s]",
+                                         properties.getProperty("foo"),
+                                         properties.getProperty("secret")));
+  }
 }
