f5595b184a710471bd3471bd565880ab605daea3
Brydon Cheyney <brydon@remission.org.uk>
2018-06-12 21:35:01 +0100

    logging in spring-boot

--- apps/spring-boot/src/main/java/app/AppController.java
+++ apps/spring-boot/src/main/java/app/AppController.java
@@ -1,11 +1,15 @@
 package app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class AppController {
+
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
   @Value("${foo}")
   private String foo;
@@ -15,6 +19,7 @@ public class AppController {
 
   @RequestMapping("/")
   public String index() {
+    logger.info("Spring Boot! [{}|{}]", foo, secret);
     return String.format("Spring Boot! [%s|%s]", foo, secret);
   }
 
